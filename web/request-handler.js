8728a4bd772e314a401ee4285052641120b7e1c3
Hack Reactor Students <accounts@hackreactor.com>
2015-07-22 15:37:02 -0700

    Passing first 3 tests for server GET in our request-handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,19 +1,41 @@
-var url = require('url');
-var path = require('path');
 var fs = require('fs');
+var path = require('path');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
 var helper = require('./http-helpers');
 
 // require more modules/folders here!
 
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "text/html"
+};
+
 exports.handleRequest = function (req, res) {
-  helper.serveAssets(res, '/index.html', function(err, file) {
-    if (err) {
-      res.writeHeader(500);
-      res.end(err);
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      helper.serveAssets(res, '/index.html', function(err, data) {
+        res.writeHead(200, headers);
+        res.end(data)
+      })
     } else {
-      res.writeHeader(200, {"Content-Type": "text/html"});
-      res.end(file);
+      var fixtureName = url.parse(req.url).pathname;
+      console.log(fixtureName);
+      console.log("Path: " + archive.paths.archivedSites);
+      fs.readFile(archive.paths.archivedSites + '/' + fixtureName, function(err, data) {
+        if (err) {
+          res.writeHead(404, headers);
+          res.end('nope');
+        } else {
+          // We found something!!!
+          res.writeHead(200, headers);
+          // res.write(JSON.stringify(fixtureName));
+          res.end(data);
+        }
+      });
     }
-  });
+  }
 };
