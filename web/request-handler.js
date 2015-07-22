3bea1d6b5218c189572bd9e0e0da4566da06389c
Warren Wong <wrrnwng@gmail.com>
2015-07-22 12:02:21 -0700

    serves index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
+var url = require('url');
 var path = require('path');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  fs.readFile(archive.paths.siteAssets + '/index.html', function(err, file) {
+    if (err) {
+      res.writeHeader(500);
+      res.write(err);
+      res.end();
+    } else {
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(file);
+      res.end();
+    }
+  });
 };
