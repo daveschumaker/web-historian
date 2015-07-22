2b521b2524d7d210387f2a4752ecd5f0f000f4f2
Warren Wong <wrrnwng@gmail.com>
2015-07-22 12:28:01 -0700

    moved functionality to http-helpers

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,19 +2,18 @@ var url = require('url');
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var helper = require('./http-helpers');
 
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  fs.readFile(archive.paths.siteAssets + '/index.html', function(err, file) {
+  helper.serveAssets(res, '/index.html', function(err, file) {
     if (err) {
       res.writeHeader(500);
-      res.write(err);
-      res.end();
+      res.end(err);
     } else {
       res.writeHeader(200, {"Content-Type": "text/html"});
-      res.write(file);
-      res.end();
+      res.end(file);
     }
   });
 };
